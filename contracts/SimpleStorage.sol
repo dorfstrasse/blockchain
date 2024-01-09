fe21ff7e589455687aad97e976aa38037a67190e
dorfstrasse <altepost173dorf07@gmail.com>
2024-01-10 02:19:18 +0700

    initial commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+    // boolean, uint, int, address, bytes
+
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 123;
+}
