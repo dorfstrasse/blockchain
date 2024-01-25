4aace95e3e21c22eeac6eadb5e8d545bb1e37287
dorfstrasse <altepost173dorf07@gmail.com>
2024-01-25 21:46:05 +0700

    addPerson

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,14 +3,28 @@ pragma solidity ^0.8.8;
 
 contract SimpleStorage {
 
-    uint256 public favoriteNumber;
+    uint256 favoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // uint256[] public favoriteNumberList;
+    People[] public people;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
 
+    // view, pure
     function retrieve() public view returns(uint256) {
         return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        // People memory newPerson = People(_favoriteNumber, _name);
+        people.push(People(_favoriteNumber, _name));
     }
 }
 
